990427926dd5590c2e2629ae93c80e74f2ed59b4
Cristofergabriel <105427788+Cristofergabriel@users.noreply.github.com>
2023-11-14 11:25:34 -0300

    ok main

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-function tocaSomClap(){
-    document.querySelector('#som_tecla_pom').play();
-    }
-    document.querySelector('.tecla_pom').onclick = tocaSompom;
+//criando/declarando a função tocaSomPom com parÂmetro idElementoAudio
+function tocaSom (idElementoAudio) {
+    document.querySelector (idElementoAudio).play();
+}
+//criando referência varável 'contador' iniciando = 0
+let contador = 0;
+//estrutura de repetição 'while' significa "enquanto algo acontecer"
+while (contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas [contador] .classList[1]
+     const idAudio = "#som_ " + efeito;
+listaDeTeclas[contador].onclick = function(){
+  tocaSom(idAudio);
+}
+contador = contador +1;
+console.log(contador);
+}
+
+
+
